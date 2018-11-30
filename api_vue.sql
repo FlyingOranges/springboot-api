d957e519c722a12896980fc118c6fb0274d76745
LeoYang <aileshang0226@163.com>
2018-11-30 22:10:08 +0800

    update -- 接口表信息变更

--- api_vue.sql
+++ api_vue.sql
@@ -1,28 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.6.38)
-# Database: api_vue
-# Generation Time: 2018-11-29 06:43:28 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table api_interface
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `api_interface`;
 
 create table `api_interface`(
@@ -41,11 +16,6 @@ create table `api_interface`(
   foreign key (`interface_project_id`) references `api_project`(`id`)
 )comment = '接口表';
 
-
-
-# Dump of table api_project
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `api_project`;
 
 CREATE TABLE `api_project` (
@@ -61,9 +31,6 @@ CREATE TABLE `api_project` (
   CONSTRAINT `api_project_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `api_users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='项目信息表';
 
-LOCK TABLES `api_project` WRITE;
-/*!40000 ALTER TABLE `api_project` DISABLE KEYS */;
-
 INSERT INTO `api_project` (`id`, `name`, `info`, `user_id`, `created_at`, `updated_at`, `status`)
 VALUES
 	(1,'1321312321','hahahahha',1,'1543200283141','1543200283141',1),
@@ -75,13 +42,6 @@ VALUES
 	(7,'就是这么尿性','怎么了，我骄傲了吗？',1,'1543201044734','1543201044734',1),
 	(8,'ali','alililili',1,'1543201095175','1543201095175',1),
 	(9,'fafdsa','fdsafdsafdsa',1,'1543279925504','1543279925504',1);
-
-/*!40000 ALTER TABLE `api_project` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table api_users
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `api_users`;
 
@@ -103,15 +63,3 @@ LOCK TABLES `api_users` WRITE;
 INSERT INTO `api_users` (`id`, `username`, `password`, `nickname`, `token`, `created_at`, `updated_at`, `status`)
 VALUES
 	(1,'aileshang0226@163.com','846e6e9c4e43735ca04c6af632ce565e','jack','59c9672dc5abd181f2aa650ca62c94c8',NULL,NULL,1);
-
-/*!40000 ALTER TABLE `api_users` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
